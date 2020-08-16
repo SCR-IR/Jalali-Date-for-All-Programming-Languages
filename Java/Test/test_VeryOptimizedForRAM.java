992f7e4dc48a072c18c052b77dbaa760986d44eb
SCR-IR <43960795+SCR-IR@users.noreply.github.com>
2020-08-16 19:47:19 +0430

    Add files via upload

--- Java/Test/test_VeryOptimizedForRAM.java
+++ Java/Test/test_VeryOptimizedForRAM.java
@@ -31,8 +31,12 @@ class Main {
       }
       
     }
+
   }
 }
+
+
+
 
 class DateConverter {
 
@@ -46,46 +50,60 @@ class DateConverter {
   1461=(365*4)+(4/4) & 146097=(365*400)+(400/4)-(400/100)+(400/400)  */
 
   public static int[] gregorian_to_jalali(int gy, int gm, int gd) {
-    int[] g_d_m = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
-    int gy2 = (gm > 2) ? (gy + 1) : gy;
-    int days = 355666 + (365 * gy) + ((int) ((gy2 + 3) / 4)) - ((int) ((gy2 + 99) / 100)) + ((int) ((gy2 + 399) / 400)) + gd + g_d_m[gm - 1];
-    int jy = -1595 + (33 * ((int) (days / 12053)));
-    days %= 12053;
-    jy += 4 * ((int) (days / 1461));
-    days %= 1461;
-    if (days > 365) {
-      jy += (int) ((days - 1) / 365);
-      days = (days - 1) % 365;
+    int[] out = {
+      (gm > 2) ? (gy + 1) : gy,
+      0,
+      0
+    };
+    {
+      int[] g_d_m = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
+      out[2] = 355666 + (365 * gy) + ((int) ((out[0] + 3) / 4)) - ((int) ((out[0] + 99) / 100)) + ((int) ((out[0] + 399) / 400)) + gd + g_d_m[gm - 1];
     }
-    int jm = (days < 186) ? 1 + (int) (days / 31) : 7 + (int) ((days - 186) / 30);
-    int jd = 1 + ((days < 186) ? (days % 31) : ((days - 186) % 30));
-    int[] jalali = { jy, jm, jd };
-    return jalali;
+    out[0] = -1595 + (33 * ((int) (out[2] / 12053)));
+    out[2] %= 12053;
+    out[0] += 4 * ((int) (out[2] / 1461));
+    out[2] %= 1461;
+    if (out[2] > 365) {
+      out[0] += (int) ((out[2] - 1) / 365);
+      out[2] = (out[2] - 1) % 365;
+    }
+    if (out[2] < 186) {
+      out[1] = 1 + (int)(out[2] / 31);
+      out[2] = 1 + (out[2] % 31);
+    } else {
+      out[1] = 7 + (int)((out[2] - 186) / 30);
+      out[2] = 1 + ((out[2] - 186) % 30);
+    }
+    return out;
   }
 
   public static int[] jalali_to_gregorian(int jy, int jm, int jd) {
     jy += 1595;
-    int days = -355668 + (365 * jy) + (((int) (jy / 33)) * 8) + ((int) (((jy % 33) + 3) / 4)) + jd + ((jm < 7) ? (jm - 1) * 31 : ((jm - 7) * 30) + 186);
-    int gy = 400 * ((int) (days / 146097));
-    days %= 146097;
-    if (days > 36524) {
-      gy += 100 * ((int) (--days / 36524));
-      days %= 36524;
-      if (days >= 365)
-        days++;
+    int[] out = {
+      0,
+      0,
+      -355668 + (365 * jy) + (((int) (jy / 33)) * 8) + ((int) (((jy % 33) + 3) / 4)) + jd + ((jm < 7) ? (jm - 1) * 31 : ((jm - 7) * 30) + 186)
+    };
+    out[0] = 400 * ((int) (out[2] / 146097));
+    out[2] %= 146097;
+    if (out[2] > 36524) {
+      out[0] += 100 * ((int) (--out[2] / 36524));
+      out[2] %= 36524;
+      if (out[2] >= 365) out[2]++;
     }
-    gy += 4 * ((int) (days / 1461));
-    days %= 1461;
-    if (days > 365) {
-      gy += (int) ((days - 1) / 365);
-      days = (days - 1) % 365;
+    out[0] += 4 * ((int) (out[2] / 1461));
+    out[2] %= 1461;
+    if (out[2] > 365) {
+      out[0] += (int) ((out[2] - 1) / 365);
+      out[2] = (out[2] - 1) % 365;
     }
-    int gd = days + 1;
-    int[] sal_a = { 0, 31, ((gy % 4 == 0 && gy % 100 != 0) || (gy % 400 == 0)) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    int gm;
-    for (gm = 0; gm < 13 && gd > sal_a[gm]; gm++) gd -= sal_a[gm];
-    int[] gregorian = { gy, gm, gd };
-    return gregorian;
+    int[] sal_a = { 0, 31, ((out[0] % 4 == 0 && out[0] % 100 != 0) || (out[0] % 400 == 0)) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    for (out[2]++; out[1] < 13 && out[2] > sal_a[out[1]]; out[1]++) out[2] -= sal_a[out[1]];
+    return out;
   }
 
 }
+
+
+
+
